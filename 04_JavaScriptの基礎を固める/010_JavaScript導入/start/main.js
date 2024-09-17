99298e4ac4cf0ad5eac108c50f2c644514b01003
fs203 <fs2031@icloud.com>
2024-09-17 19:40:03 +0900

    update

---
+++ 04_JavaScriptの基礎を固める/010_JavaScript導入/start/main.js
@@ -0,0 +1,13 @@
+// var, let, const
+
+let name = 'Tom';
+name = 'Shuichi';
+console.log('Hello' + name);
+
+// Number, String, Boolean, Undefined, Null, Symbol
+
+let variable = 'str';
+variable = 12;
+console.log(typeof variable);
+variable = false;
+variable = undefined;
