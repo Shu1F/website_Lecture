fd621c35b0e0565723a8c2cfdaccc107986704c1
fs203 <fs2031@icloud.com>
2024-09-18 22:06:39 +0900

    update

---
+++ 04_JavaScriptの基礎を固める/025_関数定義について/start/main.js
@@ -0,0 +1,11 @@
+function hello(name, age) {
+    // let name = 'Shuichi Fujiike';
+    console.log('Hello' + name + age);
+    return name + age;
+}
+
+
+
+hello(' Shuichi Fujiike', 20);
+const returnVal = hello(' Shuichi Fujiike2', 15);
+console.log(returnVal);
