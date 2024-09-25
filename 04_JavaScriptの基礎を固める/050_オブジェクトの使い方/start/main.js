ab33d9164ee98099b38667083262e65c1b8607f5
fs203 <fs2031@icloud.com>
2024-09-25 20:55:23 +0900

    update

--- 04_JavaScriptの基礎を固める/050_オブジェクトの使い方/start/main.js
+++ 04_JavaScriptの基礎を固める/050_オブジェクトの使い方/start/main.js
@@ -1,3 +1,15 @@
 const person = {
-    name: 'Shuichi Fujiike'
+    name: ['Shuichi', 'Fujiike'],
+    age: 21,
+    gender: 'male',
+    interests: {
+        sports: 'soccer',
+        music: 'piano',
+        food: 'sushi',
+    },
+    getFulName: function() {
+        console.log(this.name[0] + this.name[1])
+    }
 };
+
+person.getFulName();
