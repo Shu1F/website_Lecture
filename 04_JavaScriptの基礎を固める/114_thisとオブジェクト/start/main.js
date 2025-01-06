8fc22f94c58721584f776575ebc3b231543865e4
fs203 <fs2031@icloud.com>
2025-01-06 18:49:09 +0900

    update

--- 04_JavaScriptの基礎を固める/114_thisとオブジェクト/start/main.js
+++ 04_JavaScriptの基礎を固める/114_thisとオブジェクト/start/main.js
@@ -1,32 +1,46 @@
 const obj = {
-    first_name: 'Mafia',
-    last_name: 'Code',
-    printFullName: function() {
-        console.log(this.first_name);
-        const fn = function() {
-            console.log(this);
-        };
-        window.setTimeout(fn);  
-    }
-}
+  first_name: "Mafia",
+  last_name: "Code",
+  printFullName: function () {
+    console.log(this.first_name);
+    //呼び出した関数：.の前を指す
+    const fn = function () {
+      console.log(this);
+    };
+    window.setTimeout(fn);
+  },
+};
+
+const window = {
+  setTimeout: function (fn) {
+    fn();
+  },
+};
+
+// const obj = {
+//   first_name: "Mafia",
+//   last_name: "Code",
+//   printFullName: function () {
+//     console.log(this.first_name);
+//     const fn = function () {
+//       console.log(this);
+//     };
+//     window.setTimeout(fn);
+//   },
+// };
 
 class MyObj {
-    constructor() {
-        this.first_name = 'Mafia';
-        this.last_name = 'Code';
-    }
+  constructor() {
+    this.first_name = "Mafia";
+    this.last_name = "Code";
+  }
 
-    printFullName() {
-        console.log(this.first_name);
-        const fn = function() {
-            console.log(this);
-        };
-        window.setTimeout(fn)   
-    }
+  printFullName() {
+    console.log(this.first_name);
+  }
 }
-
+//↓ここでオブジェクト生成
 const obj2 = new MyObj();
-
 
 obj.printFullName();
 obj2.printFullName();
